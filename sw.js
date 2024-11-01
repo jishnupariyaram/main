c19c577f606f6581582e45dc0a0cfc9ad9826a06
Jishnu <57042436+jishnupariyaram@users.noreply.github.com>
2024-11-01 19:45:00 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+// Service Worker
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(clients.claim());
+});
+
+// Show notification
+self.addEventListener('push', (event) => {
+    if (event.data) {
+        const data = event.data.json();
+        event.waitUntil(
+            self.registration.showNotification(data.title, {
+                body: data.body,
+                icon: '/favicon.ico',
+                badge: '/favicon.ico',
+                data: {
+                    url: data.url
+                }
+            })
+        );
+    }
+});
+
+// Handle notification click
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    if (event.notification.data && event.notification.data.url) {
+        event.waitUntil(
+            clients.openWindow(event.notification.data.url)
+        );
+    }
+});
